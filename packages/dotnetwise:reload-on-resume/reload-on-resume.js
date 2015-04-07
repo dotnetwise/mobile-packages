ecb2bf07e3b3f116ed2f5a1974577e6719732f3c
dotnetwise <laurentiu.macovei@dotnetwise.com>
2015-04-07 13:11:27 +0300

    added hasResumed reactive var

--- packages/dotnetwise:reload-on-resume/reload-on-resume.js
+++ packages/dotnetwise:reload-on-resume/reload-on-resume.js
@@ -1,16 +1,17 @@
 var newVersionAvailable = new ReactiveVar(0);
+var hasResumed = new ReactiveVar(false);
 
-var hasResumed = false;
+
 document.addEventListener("resume", function () {
-  hasResumed = true;
+  hasResumed(true);
 }, false);
 
 Reload._onMigrate('onresume', onMigrate);
 function onMigrate(retry) {
 	console.log("hot-code-push downloaded", newVersionAvailable.get());
   newVersionAvailable.set(newVersionAvailable.get() + 1);
-	//Reload._onMigrate(onMigrate);
-  if (hasResumed) {
+  //Reload._onMigrate(onMigrate);
+  if (hasResumed.get()) {
     return [true, {}];
   } else {
     document.addEventListener("resume", retry, false);
@@ -23,6 +24,9 @@ function onMigrate(retry) {
  * the app downloaded, can be used to prompt the user to close and reopen the
  * app to get the new version.
  */
-Reload.isWaitingForResume = function () {
+Reload.hasResumed = function () {
+  return hasResumed.get();
+};
+Reload.hasResumed = function () {
   return newVersionAvailable.get();
 };
